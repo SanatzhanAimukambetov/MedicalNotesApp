1be8e1d3960d155e0d3b359265ba8bfcba746a21
Sanatzhan <sanatzhan.aimukambetov@nu.edu.kz>
2020-12-11 00:24:36 +0600

    Initial Commit

---
+++ MedicalNotesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicalNotesApp
+//
+//  Created by Sanatzhan Aimukambetov on 11.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
