f6600808866187de9c2792da5ba910e38322c782
Sanatzhan <sanatzhan.aimukambetov@nu.edu.kz>
2020-12-11 16:31:47 +0600

    Firebase was added via cocoa

---
+++ MedicalNotesApp/ConstantValues.swift
@@ -0,0 +1,21 @@
+//
+//  ConstantValues.swift
+//  MedicalNotesApp
+//
+//  Created by Sanatzhan Aimukambetov on 11.12.2020.
+//
+
+import UIKit
+
+struct ConstantValues {
+    
+    static let colorOfButton = UIColor(red: 87/255,
+                                       green: 40/255,
+                                       blue: 243/255,
+                                       alpha: 1)
+    
+    static let colorOfTextField = UIColor(red: 236/255,
+                                          green: 240/255,
+                                          blue: 243/255,
+                                          alpha: 1)
+}
---
+++ MedicalNotesApp/OnboardingView.swift
@@ -0,0 +1,69 @@
+//
+//  OnboardingView.swift
+//  MedicalNotesApp
+//
+//  Created by Sanatzhan Aimukambetov on 11.12.2020.
+//
+
+import UIKit
+import SnapKit
+
+class OnboardingView: UIView {
+    let loginTextField: UITextField = {
+       let loginTF = UITextField()
+        loginTF.backgroundColor = ConstantValues.colorOfTextField
+        return loginTF
+    }()
+    
+    let passwordTextField: UITextField = {
+        let passwordTF = UITextField()
+        passwordTF.backgroundColor = ConstantValues.colorOfTextField
+        return passwordTF
+    }()
+    
+    let signInButton: UIButton = {
+       let signInButton = UIButton()
+        signInButton.setTitle("Sign In", for: .normal)
+        signInButton.backgroundColor = ConstantValues.colorOfButton
+        return signInButton
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        addSubview(loginTextField)
+        addSubview(passwordTextField)
+        addSubview(signInButton)
+    }
+    
+    private func setupConstraints() {
+        loginTextField.snp.makeConstraints { (make) in
+            make.centerX.centerY.equalToSuperview()
+            make.trailing.leading.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        
+        passwordTextField.snp.makeConstraints { (make) in
+            make.top.equalTo(loginTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(50)
+        }
+        
+        signInButton.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(20)
+            make.height.equalTo(60)
+        }
+        
+    }
+
+}
